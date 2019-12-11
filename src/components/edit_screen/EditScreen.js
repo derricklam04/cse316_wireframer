81e1a1dd2d7eff15d6ed3daa3cde89a5d1692863
Derrick Lam <derrick.lam@stonybrook.edu>
2019-12-11 13:40:20 -0500

    added properties sidebar

--- src/components/edit_screen/EditScreen.js
+++ src/components/edit_screen/EditScreen.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux';
 import { compose } from 'redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { getFirestore } from 'redux-firestore';
+import {CompactPicker} from 'react-color';
 
 class EditScreen extends Component {
     state = {
@@ -45,10 +46,44 @@ class EditScreen extends Component {
         }
 
         return (
-            <div className="container white">
-                <h5 className="grey-text text-darken-3">Wireframe Name:</h5>
-                <div className="input-field">
-                    <input className="active" type="text" name="name" id="name" onChange={this.handleChange} value={wireFrame.name} />
+            <div className='row'>
+                <div className="grey lighten-2 col m3">
+                    <div className='row'>
+                        <i className="material-icons col m2">zoom_in</i><i className="material-icons col m2">zoom_out</i>
+                        <div className='col m4'>save</div><div className='col m4'>close</div> 
+                    </div>
+                </div>
+                <div className="white col m6 row">
+                    <h5 className="grey-text text-darken-3 col m4">Wireframe:</h5>
+                    <div className="input-field col m8">
+                        <input className="active" type="text" name="name" id="name" onChange={this.handleChange} value={wireFrame.name} />
+                    </div>
+                </div>
+                <div className ="grey lighten-2 col m3">
+                    <h5>Properties</h5>
+                    <input className="white textbox" name="textbox" placeholder="Control Text"/>
+
+                    <div className="font-size row">
+                        <h7 className="col m5">Font size:</h7>
+                        <input className="white col m6" />
+                    </div>
+                    <div className="color">
+                        <h7 className="background-color">Background:</h7>
+                        <CompactPicker/>
+                        <h7 className="text-color">Text Color:</h7>
+                        <CompactPicker/>
+                        <h7 className="border-color">Border Color:</h7>
+                        <CompactPicker/>
+                    </div>
+                    <div className="border-thickness row">
+                        <p></p>
+                        <h7 className="col m5">Border Thickness:</h7>
+                        <input className="white col m6" />
+                    </div>
+                    <div cßlassName="border-radius row">
+                        <h7 className="col m5">Border Radius:</h7>
+                        <input className="white col m6" />
+                    </div>
                 </div>
             </div>
         );
--- src/components/home_screen/WireFrameCard.js
+++ src/components/home_screen/WireFrameCard.js
@@ -21,10 +21,11 @@ class WireFrameCard extends React.Component {
         }).catch(function (error) {
             console.error("Error removing document: ", error);
         });
-
         //this.props.history.goBack();
     }
-
+    doNothing = (e) => {
+        e.stopPropagation();
+    }
     
     render() {
         const { wireFrame } = this.props;
@@ -32,17 +33,18 @@ class WireFrameCard extends React.Component {
         console.log("WireFrameCard, wireFrame.id: " + wireFrame.id);
         return (
             <div className="card z-depth-0 todo-list-link">
-                <div className="card-content grey-text text-darken-3">
-                    <span className="card-title">{wireFrame.name}</span>
-                </div> <span>  </span>
-                <h4 className="card-content btn-floating btn-large waves-effect waves-light red accent-2 hoverable modal-trigger"
-                            href="#modal1"><i className="material-icons">close</i></h4>
+                <div className="card-content grey-text text-darken-3 row">
+                        <span className="card-title col m9">{wireFrame.name}</span> 
+                        <h4 className="col m3 card-content btn-floating btn-large waves-effect waves-light red accent-2 hoverable modal-trigger"
+                                href="#modal1"><i className="material-icons">close</i></h4>
+                </div> 
+                
                 <div className="">
                     <Modal id="modal1" header="Delete List?" actions={
                         <div className="grey lighten-2">
                             <Button className="red accent-2" tooltip="The list will not be retrievable." tooltipOptions={{ position: 'top' }}
                                 onClick={handleClick} modal="close">Yes</Button><span>  </span>
-                            <Button className="purple lighten-2" modal="close">No</Button>
+                            <Button onClick={this.doNothing} className="purple lighten-2" modal="close">No</Button>
                         </div>}>
                         <p><b>Are you sure you want to delete this list?</b></p>
                     </Modal>
